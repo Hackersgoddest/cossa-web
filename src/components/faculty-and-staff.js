c8aade38fe8cad5f155eef81b3bdc8b1e282fceb
Oppong Frank Konadu <frankoppongkonadu930@gmail.com>
2024-01-15 23:06:45 +0000

    [FE] moved faculty and staffs information to a seperate js file

---
+++ src/components/faculty-and-staff.js
@@ -0,0 +1,49 @@
+const staffs = [
+  {
+    name: "John Kingsley Arthur",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/kingsley-arthur.jpg",
+  },
+  {
+    name: "J. Abandoh-sam",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/J. Abandoh-sam.jpg",
+  },
+  {
+    name: "Prince Yaw Owusu Amoako",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/Prince Yaw Owusu Amoako.jpg",
+  },
+  {
+    name: "A. Salako",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/a. Salako.jpg",
+  },
+  {
+    name: "Kingsley Arthur",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/kingsley-arthur.jpg",
+  },
+  {
+    name: "Kingsley Arthur",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/kingsley-arthur.jpg",
+  },
+  {
+    name: "Kingsley Arthur",
+    position: "Senior Lecture",
+    imageUrl: "/about/faculty-and-staff/kingsley-arthur.jpg",
+  },
+  {
+    name: "Kingsley Arthur",
+    position: "Senior Lecturer",
+    imageUrl: "/about/faculty-and-staff/kingsley-arthur.jpg",
+  },
+  {
+    name: "Micheal kelly",
+    position: "Junior Lecturer",
+    imageUrl: "/about/faculty-and-staff/Micheal kelly.jpg",
+  },
+];
+
+export default staffs;
